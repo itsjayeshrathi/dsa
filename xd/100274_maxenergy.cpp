0a7f9ac2c767a3e4973405fb57da434f611851e7
Jayesh Rathi <itsjayeshrathi@gmail.com>
2024-05-13 23:15:44 +0530

    few leetcode problems

---
+++ xd/100274_maxenergy.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// int maximumEnergy(vector<int> &energy, int k)
+// {
+//     int maxEnergy = INT_MIN;
+//     for (int i = 0; i < energy.size(); i++)
+//     {
+//         int temp = energy[i];
+//         for (int j = i + k; j < energy.size(); j += k)
+//         {
+//             temp += energy[j];
+//         }
+
+//         maxEnergy = max(temp, maxEnergy);
+//     }
+//     return maxEnergy;
+//
+// }
+
+int main()
+{
+    vector<int> v = {-2, -3, -1};
+    int k = 2;
+    //int maxEnergy = maximumEnergy(v, k);
+    //cout << maxEnergy << endl;
+    return 0;
+}
